0d5e14c186b73d1bc8404e1a03b0a74c1d87fbee
CorentinLimier <corentin.limier@gmail.com>
2016-12-27 01:15:02 +0100

    fix datatables + add jquery toggle to sidebars a href onclick

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,22 @@
 (function() {
 	'use strict';
 
+	var toggleSection = function(e){
+		e.preventDefault();
+		$("#scenarios_list").toggle();
+		$("#new_scenario").toggle();
+	};
+
 	$(function(){
-		$("#scenarios_table").DataTable();
+		// DataTables
+		$("#new_scenario").toggle();
+		$("#scenarios_table").DataTable({
+			'pageLength': 50
+		});
+
+		// Sidebar
+		$("#link_scenarios_list").click(toggleSection);
+		$("#link_new_scenario").click(toggleSection);
 	});
 }());
 
