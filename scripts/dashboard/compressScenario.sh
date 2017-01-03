a4d3cc1b7456967dd311f7dc86b408b61c6a7dd2
CorentinLimier <corentin.limier@gmail.com>
2017-01-03 19:26:23 +0100

    fix script compressScenario.sh

--- scripts/dashboard/compressScenario.sh
+++ scripts/dashboard/compressScenario.sh
@@ -5,6 +5,6 @@ scenarioId=$2
 
 current=$(pwd)
 
-cd $scenariosPath
-tar -cjf "${scenarioId}.tar.bz2" $scenarioId
+cd $scenariosPath && \
+tar -cjf "${scenarioId}.tar.bz2" $scenarioId &&\
 cd $current
